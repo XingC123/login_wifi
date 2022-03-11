102996a912183ba5d993272f44b220c5f2737202
XingC <123fengmo@gmail.com>
2022-03-11 18:25:47 +0800

    Update the setting rules of version number

--- GUI/app_info.py
+++ GUI/app_info.py
@@ -7,7 +7,7 @@ def info_app_info_menu(window):
     '''
     version: 第1位: 大版本号
              第2位: 小版本号
-             第3位: commit次数
+             第3位: 面向用户性commit次数 (不包括"更新版本号"之类的commit)
              括号: 总发布版本数 (从 2022-3-11 加入此规则, 遂从这一次版本开始计算)
     '''
     version = '版本: 2.1.3 (1)'
